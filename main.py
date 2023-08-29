196b46c1c24d7b87291f4efacc006eac9c6a8efa
AlexCuenta <cuentafalsa97a@gmail.com>
2023-08-28 20:09:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+from uce.ai.openuce import Document, process_inference
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.post("/inference")
+def inference(doc: Document):
+    ingredientes = process_inference(doc.item)
+    return {
+        'response': ingredientes
+    }
+
+if __name__ == "__main__":
+    app.run()
---
+++ uce/ai/openuce.py
@@ -0,0 +1,29 @@
+import openai
+from pydantic import BaseModel
+
+openai.organization = 'org-vgmFNY6C1ojYlplUUiJYB0vi'
+openai.api_key = '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'
+
+class Document(BaseModel):
+    item: str = 'pizza'
+
+def process_inference(user_prompt) -> str:
+    print('[PROCESANDO]'.center(40, '-'))
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": """Eres un chef que lista los ingredientes de los platillos que se te proporcionan.
+        E.G
+        pan
+        Ingredientes:
+        harina
+        huevos
+        agua
+        azucar
+        ...
+        """},
+            {"role": "user", "content": user_prompt}
+        ]
+    )
+    response = completion.choices[0].message.content
+    return response
